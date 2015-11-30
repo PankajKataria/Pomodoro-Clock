ed1fb279f290ec78c702bd9adc90b0fba42e0f82
Pankaj Kataria <codeuchiha@gmail.com>
2015-11-30 08:25:46 +0530

    Finished Basic Functionality

--- javascript/app.js
+++ javascript/app.js
@@ -1,80 +1,94 @@
 var app=angular.module('mainapp',[]);
 app.controller('mainCtrl',function($scope,$interval){
   $scope.breakLength=5;
-  $scope.intervalLength=25;
+  $scope.intervalLength=1;
   $scope.timeLeft=tohms($scope.intervalLength*60);
+  $scope.sessionName='interval';
   var runTimer=false;
   var timeInSeconds=$scope.intervalLength*60;
-
-  
+  var runMuzic=true;
+  function paddedzero(num){
+    if(num > 9 ){
+      return num.toString();
+    }
+    else{
+      return '0'+num.toString();
+    }
+  }
   function tohms(seconds){
-    if(seconds > 0){
-        var hours= Math.floor(seconds/3600);
-        var min  = Math.floor(seconds/60);
-        var sec  = seconds%60;
+      if(seconds >= 0){
+        var result='';
+        var hours = Math.floor(seconds/3600);
+        var min   = Math.floor(seconds%3600/60);
+        var sec   = seconds%60;
         if(hours > 0){
-          if(sec > 9 ){
-              return hours+':'+min+':'+ sec;
-          }
-          else{
-            return hours+':'+min+':0'+ sec;
-          }
-        }
-        else if ( min > 0){
-          if(sec > 9 ){
-              return min+':'+ sec;
-          }
-          else{
-            return min+':0'+ sec;
-          }
+          return paddedzero(hours)+':'+paddedzero(min)+':'+paddedzero(sec);
         }
         else{
-          if(sec > 9 ){
-              return '00:'+sec;
-          }
-          else{
-            return '00:0'+ sec;
-          }
+          return paddedzero(min)+':'+paddedzero(sec);
         }
-    }
+      }
   }
   $scope.toggle=function(d,type){
     if(!isNaN(d)){
       if(d == 1 | d == -1 ){
         if( type === 'break'){
-            if($scope.breakLength > 0 | d == 1){
+          if(($scope.breakLength > 0 | d == 1)){
+            if(!runTimer){
+              $scope.breakLength+=d;
+              if($scope.sessionName === 'break'){
+                $scope.timeLeft=tohms($scope.breakLength*60);
+                timeInSeconds=$scope.breakLength*60;
+              }
+            }
+            else if(runTimer && $scope.sessionName === 'interval'){
               $scope.breakLength+=d;
             }
+          }
         }
         else if(type == 'interval'){
-          if(($scope.intervalLength > 0 | d == 1)&& !runTimer){
-            $scope.intervalLength+=d;
-            $scope.timeLeft=tohms($scope.intervalLength*60);
-            timeInSeconds=$scope.intervalLength*60;
+          if(($scope.intervalLength > 0 | d == 1)){
+            if(!runTimer){
+              $scope.intervalLength+=d;
+              if($scope.sessionName == 'interval'){
+                $scope.timeLeft=tohms($scope.intervalLength*60);
+                timeInSeconds=$scope.intervalLength*60;
+              }
+            }
+            else if(runTimer && $scope.sessionName === 'break'){
+              $scope.intervalLength+=d;
+            }
           }
         }
       }
     }
   }
-  $scope.toggleTimer=function(){
-    if(runTimer){
-      $interval.cancel(runTimer);
-      runTimer=false;
-    }
-    else {
-      runTimer=$interval(updateTimer,1000);
-    }
-  }
 
+  $scope.startTimer=function(){
+    if(runTimer){
+      return;
+    }
+    runTimer=$interval(updateTimer,1000);
+  }
   function updateTimer(){
     timeInSeconds-=1;
-
     if(timeInSeconds < 0){
-      $scope.timeLeft='funckit';
+      if($scope.sessionName === 'interval'){
+        $scope.sessionName='break';
+        timeInSeconds=60*$scope.breakLength;
+      }
+      else if($scope.sessionName === 'break'){
+        $scope.sessionName='interval';
+        timeInSeconds=60*$scope.intervalLength;
+      }
     }
-
     $scope.timeLeft=tohms(timeInSeconds);
   }
-
-
+  $scope.stopTimer=function(){
+    if(!runTimer){
+      return;
+    }
+    $interval.cancel(runTimer);
+    runTimer=false;
+  }
 });
